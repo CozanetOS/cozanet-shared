758ad8c566d365e14831c3468b6c88fcd432707b
CozyCrypto <fassdavid722@gmail.com>
2026-07-10 23:17:58 +0100

    Initial scaffold

---
+++ src/Interfaces/IMemoryStore.ts
@@ -0,0 +1,6 @@
+export interface IMemoryStore {
+  get(key: string): Promise<any>;
+  set(key: string, value: any, ttl?: number): Promise<boolean>;
+  delete(key: string): Promise<boolean>;
+  clear(): Promise<void>;
+}
