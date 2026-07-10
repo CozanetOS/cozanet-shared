d835a9c4c4ae8d5ac43d315693884fbaee24eb06
CozyCrypto <fassdavid722@gmail.com>
2026-07-10 23:18:00 +0100

    Initial scaffold

---
+++ src/Constants/eventTypes.ts
@@ -0,0 +1,8 @@
+export const EVENT_TYPES = {
+  AGENT_REGISTERED: 'event:agent_registered',
+  TASK_CREATED: 'event:task_created',
+  TASK_COMPLETED: 'event:task_completed',
+  TASK_FAILED: 'event:task_failed',
+  SYSTEM_BOOT: 'event:system_boot',
+  SYSTEM_SHUTDOWN: 'event:system_shutdown'
+} as const;
